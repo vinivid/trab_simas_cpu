c35cbcadb7be8deeefcdf09ca261d72f3265a10b
vinivid <frato.vini@gmail.com>
2025-06-15 12:08:53 -0300

    Outra forma de desenhar quatro por quatro

--- jogo_asm/Map.asm
+++ jogo_asm/Map.asm
@@ -2,7 +2,4 @@
 ; ao mapa e funcoes para interagir com o mapa.
 
 @include util.asm
-
-;   O tile grid eh um array que repsenta as tiles do mapa,
-; em uma grid 16 x 13
-tile_grid : var #208
+@include mapa.asm
--- jogo_asm/util.asm
+++ jogo_asm/util.asm
@@ -1,10 +1,57 @@
+;   Desenha na posicao da tela o char repetidias
+; vezes em um padrao 2 x 2.
+;
+; Parametros:
+;
+; @param {char} r0 - Eh o char que deseja  
+;
+; @param {int} r1 - Eh a posicao na tela em que 
+; deseja desenhar o sprite. Tendo em mente que 
+; o sprite tera o seu canto superior esquerdo
+; nessa posicao.
+;
+;  Ex: r0 = 'A' 
+;      r1 = #0
+;
+; Desenha:
+;
+;   AA 
+;   AA 
+;
+two_by_two_draw:
+    ; Salvando as registradoras
+    ; usadas nessa funcao que nao sao argumento
+	push r3 
+	push r4
+	
+	loadn r3, #1  ; Seta o step de uma coluna
+	loadn r4, #39 ; Sera o step de uma linha da tela
+	
+	outchar r0, r1 ; primeira posicao 
+	
+	; desenhando na direita da primeira pos
+	add r1, r1, r3
+	outchar r0, r1
+  
+    ; desenhando na posicao abaixo da primeira pos
+	add r1, r1, r4 ; pulando a linha da tela
+	outchar r0, r1
+  
+    ; desenha na diagonal para baixo e direita 
+    ; da primeira
+	add r1, r1, r3
+	outchar r0, r1
+
+	pop r4
+	pop r3
+
 ;   Desenha na posicao da tela a sequencia
 ; de quatro chars que representam um sprite
 ; 2x2.
 ;
 ; Parametros:
 ;
-; @param {endereco} r0 - Eh o primeiro char da 
+; @param {char} r0 - Eh o primeiro char da 
 ; sequencia de caracteres que deseja desenhar
 ; na tela.
 ; @param {int} r1 - Eh a posicao na tela em que 
@@ -12,7 +59,15 @@
 ; o sprite tera o seu canto superior esquerdo
 ; nessa posicao.
 ;
-two_by_two_draw:
+;  Ex: r0 = 'A' 
+;      r1 = #0
+;
+; Desenha:
+;
+;   AB 
+;   CD 
+;
+two_by_two_sequence_draw:
     ; Salvando as registradoras
     ; usadas nessa funcao que nao sao argumento
 	push r3
