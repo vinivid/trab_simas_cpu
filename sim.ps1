a234d1bd1bc4e92df9aa545abd4076d093c66cb0
vinivid <frato.vini@gmail.com>
2025-06-27 18:25:58 -0300

    Coisas terriveis

--- sim.ps1
+++ sim.ps1
@@ -2,5 +2,5 @@ Copy-Item -Force .\Build\cpuram.mif .\Simulador\Versao_05_2013\32_bits\cpuram.mi
 Copy-Item -Force .\Build\charmap.mif .\Simulador\Versao_05_2013\32_bits\charmap.mif
 Push-Location
 Set-Location .\Simulador\Versao_05_2013\32_bits\
-.\simulador_Atual_Funciona.exe cpuram.mif charmap.mif
+.\simulador_Atual.exe cpuram.mif charmap.mif
 Pop-Location
