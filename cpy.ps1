83b4d09bba8b04cbcf317f5e6e7e3340cdbf84c3
vinivid <frato.vini@gmail.com>
2025-06-27 16:49:13 -0300

    Testar na fpga

--- cpy.ps1
+++ cpy.ps1
@@ -1,3 +1,7 @@
 Set-Location .\jogo_asm
 py .\include.py .\SimasBomber.asm ..\Build\tudo.asm
 Set-Location ..
+.\Simulador\Versao_05_2013\32_bits\Montador.exe .\Build\tudo.asm .\Build\CPURAM.MIF
+Copy-Item -Force .\jogo_asm\charmap.mif .\Build\charmap.mif
+Copy-Item -Force .\Build\charmap.mif .\cpu\CPURAM.mif
+Copy-Item -Force .\Build\charmap.mif .\cpu\charmap.mif
